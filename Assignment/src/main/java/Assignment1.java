03cf278c5e5082378a3439d463ef1e111d7768a9
siddhartha-ReportGarden <34242041+siddhartha-ReportGarden@users.noreply.github.com>
2017-12-05 16:11:25 +0530

    Update Assignment1.java

--- Assignment/src/main/java/Assignment1.java
+++ Assignment/src/main/java/Assignment1.java
@@ -1,8 +1,11 @@
-package main.java;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 import java.io.IOException;
 import java.sql.*;
@@ -10,9 +13,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Iterator;
+import java.util.Set;
 
-
-public class Assignment1 {
+public class Testing {
 
 
     private OkHttpClient client = new OkHttpClient();
@@ -42,42 +46,49 @@ public class Assignment1 {
     }
 
 
-    private static void searchFunc(String repo) throws IOException {
+    private static void searchFunc(String repo) throws JSONException {
         String url = "https://api.github.com/search/repositories?q=";
         url = url+ repo;
         //System.out.println(url+"--------------"+repo);
 
-        Assignment1 example = new Assignment1();
-        String rate= example.run(url);
-        System.out.println(rate);
-
-        List<String> owners = new ArrayList<>();
-        Arrays.stream(rate.split("\"full_name\":")).skip(1).map(l -> l.split(",")[0].split("/")[0].split("\"")[1]).forEach(owners::add);
-
-        List<String> repoNames = new ArrayList<>();
-        Arrays.stream(rate.split("\"full_name\":")).skip(1).map(l -> l.split(",")[0].split("/")[1].split("\"")[0]).forEach(repoNames::add);
-
-        List<String> starCounts = new ArrayList<>();
-        Arrays.stream(rate.split("\"stargazers_count\":")).skip(1).map(l -> l.split(",")[0]).forEach(starCounts::add);
-
-        List<String> forkCounts = new ArrayList<>();
-        Arrays.stream(rate.split("\"forks\":")).skip(1).map(l -> l.split(",")[0]).forEach(forkCounts::add);
-
-        for(int i=0;i<owners.size();i++){
-            System.out.println("owner = "+owners.get(i)+"\t repo name = "+repoNames.get(i)+"\t star count = "+starCounts.get(i)+"\t fork count = "+forkCounts.get(i));
+        Testing example = new Testing();
+        String rate="";
+        try {
+            rate = example.run(url);
         }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        //System.out.println(rate);
+
+        JSONObject obj = new JSONObject(rate);
+
+        JSONArray allItems = obj.getJSONArray("items");
+
+        for(int itr =0;itr< allItems.length(); itr++){
+            JSONObject itrObj = (JSONObject)allItems.get(itr);
+            String name = (String)itrObj.get("full_name");
+            name = name.split("/")[0];
+
+            System.out.printf("owner = %-20s repo name = %-20s star count = %-10s fork count = %-10s \n",name,itrObj.get("name"),itrObj.get("stargazers_count"),itrObj.get("forks"));
+
+        }
+        System.out.println("total: " + obj.get("total_count"));
+
     }
 
 
-    private static  void importFunc(String ID) throws IOException {
+    private static  void importFunc(String ID) throws IOException, JSONException {
         String url1 = "https://api.github.com/repositories/" + ID;
-        Assignment1 example = new Assignment1();
+        Testing example = new Testing();
         //retrieving the fullname/repositoryname
 
         System.out.println(url1);
         String repoDetails = example.run(url1);
+        JSONObject obj = new JSONObject(repoDetails);
+        repoDetails = (String) obj.get("full_name");
 
-        repoDetails = repoDetails.split("\"full_name\":")[1].split(",")[0].split("\"")[1].split("\"")[0];
+        //repoDetails = repoDetails.split("\"full_name\":")[1].split(",")[0].split("\"")[1].split("\"")[0];
         System.out.println(repoDetails);
 
         //7796523 for sample testing
@@ -85,25 +96,34 @@ public class Assignment1 {
 
         String url = "https://raw.githubusercontent.com/" + repoDetails + "/master/package.json";
         String rate = example.run(url);
+        System.out.println(url);
 
         if (!rate.equals("404: Not Found\n")) {
             List<String> packages = new ArrayList<>();
-            String res = rate.split("\"dependencies\": ")[1];
-            res = res.split("bugs")[0];
+            JSONObject packageObj = new JSONObject(rate);
+            JSONObject packagesJSON = packageObj.getJSONObject("dependencies");
+            Iterator keys = packagesJSON.keys();
 
-            res = res.substring(res.indexOf("{") + 1);
-            res = res.substring(0, res.indexOf("}"));
-            Arrays.stream(res.split(",")).map(l -> l.split(",")[0].split(":")[0]).forEach(packages::add);
+
+            //Iterator a = keys.iterator();
+            while(keys.hasNext()) {
+                String key = (String)keys.next();
+                packages.add(key);
+                // loop to get the dynamic key
+                String value = (String)packagesJSON.get(key);
+                System.out.printf("key = %-25s value = %-25s \n",key,value);
+            }
+
+
+
+            //System.out.println("+++++"+packagesJSON+"+++++++");
+
             StringBuilder allPackages = new StringBuilder();
             for (String installed_package : packages) {
                 allPackages.append(installed_package).append(",");
             }
             allPackages = new StringBuilder(allPackages.substring(0, allPackages.length() - 2));
-            System.out.println("-----------------------------");
-            for (String installed_package : packages) {
-                System.out.print(installed_package + " ,");
-            }
-            System.out.println("\n-----------------------------");
+
 
             Connection cDB = getConnect();
             if (cDB != null) {
@@ -118,8 +138,6 @@ public class Assignment1 {
                 }
 
                 for (String installed_package : packages) {
-
-                    installed_package = installed_package.split("\"")[1].split("\"")[0];
                     sql = "SELECT PACKAGE_COUNT FROM PACKAGE_TABLE WHERE PACKAGE_NAME = '" + installed_package + "';";
                     try {
                         assert st != null;
@@ -165,7 +183,7 @@ public class Assignment1 {
                 while (rs.next()){
                     String package_name = rs.getString("package_name");
                     int package_count = rs.getInt("package_count");
-                    System.out.println(package_name+" : " + package_count);
+                    System.out.printf("%-25s : %10d\n",package_name,package_count);
                 }
 
             } catch (SQLException e) {
@@ -174,29 +192,31 @@ public class Assignment1 {
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        System.out.println("Hi there\n Select 1 for Searching \n Select 2 for importing \n Select 3 for Toppacks");
+    public static void main(String[] args) throws IOException, JSONException {
+        while(true) {
+            System.out.println("\n\nHi there\n Select 1 for Searching \n Select 2 for importing \n Select 3 for Toppacks \n Select 4 to Exit");
 
-        Scanner in = new Scanner(System.in);
-        int choice;
-        choice = in.nextInt();
-        if(choice == 1 ){
-            System.out.println("Please Enter the Keyword for searching");
-            String searchWord = in.next();
-            searchFunc(searchWord);
-        }
-        else if(choice == 2){
-            System.out.println("Please Enter the 'GitHub Repository id' to import the packages");
-            String ID =in.next();
-            importFunc(ID);
-        }
-        else if(choice == 3){
-            System.out.println("Here are your TopPacks");
-            topPacks();
+            Scanner in = new Scanner(System.in);
+            int choice;
+            choice = in.nextInt();
+            if (choice == 1) {
+                System.out.println("Please Enter the Keyword for searching");
+                String searchWord = in.next();
+                searchFunc(searchWord);
+            } else if (choice == 2) {
+                System.out.println("Please Enter the 'GitHub Repository id' to import the packages");
+                String ID = in.next();
+                importFunc(ID);
+            } else if (choice == 3) {
+                System.out.println("Here are your TopPacks");
+                topPacks();
 
-        }
-        else {
-            System.out.println("Enter a value among 1,2 or 3");
+            } else if( choice == 4){
+                System.out.println("Tata..See you again");
+                break;
+            } else {
+                System.out.println("Enter a value among 1,2, 3 or 4");
+            }
         }
     }
 
