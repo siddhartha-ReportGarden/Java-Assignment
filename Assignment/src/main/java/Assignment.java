8c1c48c063f156fd738c2d94571ae29f6755a4f0
siddhartha-ReportGarden <34242041+siddhartha-ReportGarden@users.noreply.github.com>
2017-12-06 17:35:09 +0530

    Update Assignment.java

--- Assignment/src/main/java/Assignment.java
+++ Assignment/src/main/java/Assignment.java
@@ -1,3 +1,10 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -7,20 +14,17 @@ import org.json.JSONObject;
 
 
 import java.io.IOException;
-import java.sql.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Iterator;
-import java.util.Set;
 
 public class Assignment {
 
 
   private OkHttpClient client = new OkHttpClient();
 
-  private static Connection getConnect() {
+  private Connection getConnect() {
     Connection conn = null;
     try {
       Class.forName("org.postgresql.Driver");
@@ -32,15 +36,47 @@ public class Assignment {
     return conn;
   }
 
-  private static void searchFunc(String repo) {
-    String url = "https://api.github.com/search/repositories?q=";
-    url = url + repo;
-    //System.out.println(url+"--------------"+repo);
+  private String Run(String url) throws IOException {
+    Request request = new Request.Builder()
+        .url(url)
+        .build();
+
+      Response response = client.newCall(request).execute();
+      return response.body().string();
+  }
+
+
+  private void SearchFunc(String repo, int page, int pageLimit) {
+    if(repo.equals("")){
+      System.out.println("Enter a valid search keyword");
+      return;
+    }
+    String url = "https://api.github.com/rate_limit";
 
     Assignment example = new Assignment();
-    String rate = "";
+    String rate;
     try {
-      rate = example.run(url);
+      rate = example.Run(url);
+      JSONObject obj = new JSONObject(rate);
+
+      JSONObject rateValue = obj.getJSONObject("resources");
+      rateValue = rateValue.getJSONObject("search");
+
+      int remaining = rateValue.getInt("remaining");
+      if (remaining == 0) {
+        System.out.println("The rate limit is completed. Please try after a while");
+        return;
+      }
+    } catch (IOException | JSONException e) {
+      System.out.println(e);
+      return ;
+    }
+
+    url = "https://api.github.com/search/repositories?q=";
+    url = url + repo+"&page="+page+"&per_page="+pageLimit;
+    System.out.println(url);
+    try {
+      rate = example.Run(url);
       JSONObject obj = new JSONObject(rate);
       JSONArray allItems = obj.getJSONArray("items");
 
@@ -53,9 +89,7 @@ public class Assignment {
                 name, itrObj.get("name"), itrObj.get("stargazers_count"), itrObj.get("forks"));
       }
 
-    } catch (IOException e) {
-      e.printStackTrace();
-    } catch (JSONException e) {
+    } catch (IOException | JSONException e) {
       System.out.println(e);
     }
 
@@ -63,23 +97,49 @@ public class Assignment {
 
   }
 
-  private static void importFunc(String id) {
-    String url1 = "https://api.github.com/repositories/" + id;
+  private void ImportFunc(String id) {
+    if(id.equals("")){
+      System.out.println("Enter a valid repo ID");
+      return;
+    }
+
+    String url = "https://api.github.com/rate_limit";
+
     Assignment example = new Assignment();
-    //retrieving the fullname/repositoryname
+    String rate;
+    try {
+      rate = example.Run(url);
+      JSONObject obj = new JSONObject(rate);
+
+      JSONObject rateValue = obj.getJSONObject("resources");
+      rateValue = rateValue.getJSONObject("core");
+
+      int remaining = rateValue.getInt("remaining");
+      if (remaining == 0) {
+        System.out.println("The rate limit is completed. Please try after a while");
+        return;
+      }
+    } catch (IOException | JSONException e) {
+      System.out.println(e);
+      return ;
+    }
+
+    String url1 = "https://api.github.com/repositories/" + id;
 
     System.out.println(url1);
     String repoDetails = "";
     try {
-      repoDetails = example.run(url1);
+      repoDetails = example.Run(url1);
     } catch (IOException e) {
       System.out.println(e);
+      return;
     }
     try {
       JSONObject obj = new JSONObject(repoDetails);
       repoDetails = (String) obj.get("full_name");
     } catch (JSONException e) {
       System.out.println(e);
+      return;
     }
 
     //repoDetails = repoDetails.split("\"full_name\":")[1].split(",")[0].split("\"")[1].split("\"")[0];
@@ -87,12 +147,12 @@ public class Assignment {
 
     //7796523 for sample testing
 
-    String url = "https://raw.githubusercontent.com/" + repoDetails + "/master/package.json";
-    String rate = "";
+    url = "https://raw.githubusercontent.com/" + repoDetails + "/master/package.json";
     try {
-      rate = example.run(url);
+      rate = example.Run(url);
     } catch (IOException e) {
       System.out.println(e);
+      return;
     }
 
     System.out.println(url);
@@ -117,6 +177,7 @@ public class Assignment {
 
       } catch (JSONException e) {
         System.out.println(e);
+        return;
       }
 
       allPackages = new StringBuilder(allPackages.substring(0, allPackages.length() - 2));
@@ -128,41 +189,42 @@ public class Assignment {
             "INSERT INTO IMPORT_TABLE (REPOID, REPO_OWNER, REPO_NAME, REPO_PACKAGES ) VALUES(" + id
                 + ",'" + repoDetails.split("/")[0] + "','" + repoDetails.split("/")[1] + "','"
                 + allPackages + "')";
-
         try {
           st = cDB.createStatement();
           st.executeUpdate(sql);
         } catch (SQLException e) {
           e.printStackTrace();
+          return;
         }
 
-        for (String installed_package : packages) {
-          sql = "SELECT PACKAGE_COUNT FROM PACKAGE_TABLE WHERE PACKAGE_NAME = '" + installed_package
+        for (String installedPackage : packages) {
+          sql = "SELECT PACKAGE_COUNT FROM PACKAGE_TABLE WHERE PACKAGE_NAME = '" + installedPackage
               + "';";
           try {
-            assert st != null;
-            ResultSet rs = st.executeQuery(sql);
 
-            int count = 0;
-            while (rs.next()) {
-              count = rs.getInt("PACKAGE_COUNT");
+              ResultSet rs = st.executeQuery(sql);
+              System.out.println("Error establishing connection to database");
+
+              int count = 0;
+              while (rs.next()) {
+                count = rs.getInt("PACKAGE_COUNT");
+              }
+
+              if (count == 0) {
+                sql = "INSERT INTO PACKAGE_TABLE (PACKAGE_NAME, PACKAGE_COUNT) VALUES('"
+                    + installedPackage + "', 1);";
+                st.executeUpdate(sql);
+              } else {
+                count += 1;
+                sql = "UPDATE PACKAGE_TABLE SET PACKAGE_COUNT =" + count + " WHERE PACKAGE_NAME='"
+                    + installedPackage + "';";
+                st.executeUpdate(sql);
+              }
+
+            } catch (SQLException e) {
+              e.printStackTrace();
+              return;
             }
-
-            if (count == 0) {
-              sql = "INSERT INTO PACKAGE_TABLE (PACKAGE_NAME, PACKAGE_COUNT) VALUES('"
-                  + installed_package + "', 1);";
-              st.executeUpdate(sql);
-            } else {
-
-              count += 1;
-              sql = "UPDATE PACKAGE_TABLE SET PACKAGE_COUNT =" + count + " WHERE PACKAGE_NAME='"
-                  + installed_package + "';";
-              st.executeUpdate(sql);
-            }
-
-          } catch (SQLException e) {
-            e.printStackTrace();
-          }
 
         }
       } else {
@@ -172,7 +234,7 @@ public class Assignment {
 
   }
 
-  private static void topPacks() {
+  private  void TopPacks() {
     Connection cDB = getConnect();
     if (cDB != null) {
       Statement st = null;
@@ -193,25 +255,49 @@ public class Assignment {
   }
 
   public static void main(String[] args) {
+    Assignment assignment = new Assignment();
     while (true) {
       System.out.println(
           "\n\nHi there\n Select 1 for Searching \n Select 2 for importing \n Select 3 for Toppacks \n Select 4 to Exit");
-
       Scanner in = new Scanner(System.in);
       int choice;
       choice = in.nextInt();
       if (choice == 1) {
         System.out.println("Please Enter the Keyword for searching");
         String searchWord = in.next();
-        searchFunc(searchWord);
+        int page = 1;
+        int pageLimit;
+        System.out.println("Please enter the number of entries you want per page");
+        pageLimit = in.nextInt();
+        assignment.SearchFunc(searchWord,page,pageLimit);
+        String moreResults;
+
+        label:
+        while (true) {
+          System.out.println("press Y for more results. Else click N");
+          moreResults = in.next();
+          switch (moreResults) {
+            case "Y":
+            case "y":
+              page += 1;
+              assignment.SearchFunc(searchWord, page, pageLimit);
+              break;
+            case "N":
+            case "n":
+              break label;
+            default:
+              System.out.println("Please enter a valid answer. Either Y or N");
+              break;
+          }
+        }
+        //SearchFunc(searchWord);
       } else if (choice == 2) {
         System.out.println("Please Enter the 'GitHub Repository id' to import the packages");
-        String ID = in.next();
-        importFunc(ID);
+        String id = in.next();
+        assignment.ImportFunc(id);
       } else if (choice == 3) {
         System.out.println("Here are your TopPacks");
-        topPacks();
-
+        assignment.TopPacks();
       } else if (choice == 4) {
         System.out.println("Tata..See you again");
         break;
@@ -220,18 +306,6 @@ public class Assignment {
       }
     }
   }
-
-  String run(String url) throws IOException {
-    Request request = new Request.Builder()
-        .url(url)
-        .build();
-
-    try (Response response = client.newCall(request).execute()) {
-      assert response.body() != null;
-      return response.body().string();
-    }
-  }
-
 }
 
 //7796523
